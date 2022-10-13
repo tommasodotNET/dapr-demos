7e0a89a16ab02d30dbf3322d44fa0bc7f620def1
Tommaso Stocchi <tommasostocchi@outlook.com>
2022-10-13 14:21:42 +0200

    adds native http cliente to service2service

--- src/service-invocation/Client/Program.cs
+++ src/service-invocation/Client/Program.cs
@@ -2,43 +2,86 @@
 using Dapr.Client;
 using MyModel;
 
+Console.WriteLine("============ HTTP Client ============");
+Console.WriteLine("Calling using HttpClient...");
+await ReadUsingHttpClientAsync();
+Console.WriteLine("Done.");
+Console.WriteLine("=====================================");
+
+Console.WriteLine();
+
+Console.WriteLine("========= Dapr HTTP Client ==========");
 Console.WriteLine("Calling using DaprHttpClient...");
 await ReadUsingHttpDaprClientAsync();
 Console.WriteLine("Done.");
+Console.WriteLine("=====================================");
 
+Console.WriteLine();
+
+Console.WriteLine("=========== Dapr Client ============");
 Console.WriteLine("Calling using DaprClient...");
 await ReadUsingDaprClientAsync();
 Console.WriteLine("Done.");
+Console.WriteLine("====================================");
+
+async Task ReadUsingHttpClientAsync()
+{
+    using var httpClient = new HttpClient();
+
+    var daprPort = Environment.GetEnvironmentVariable("DAPR_HTTP_PORT") ?? "3500";
+    Console.WriteLine($"DAPR_HTTP_PORT: {daprPort}");
+    var baseUrl = $"http://localhost:{daprPort}/v1.0";
+    var appId = "server";
+
+    var helloResponse = new HttpResponseMessage();
+    var helloMessage = "";
+
+    helloResponse = await httpClient.GetAsync($"{baseUrl}/invoke/{appId}/method/");
+    helloResponse.EnsureSuccessStatusCode();
+    helloMessage = await helloResponse.Content.ReadAsStringAsync();
+    Console.WriteLine(helloMessage);
+
+    var name = "Tommaso";
+    helloResponse = await httpClient.GetAsync($"{baseUrl}/invoke/{appId}/method/hello/{name}");
+    helloResponse.EnsureSuccessStatusCode();
+    helloMessage = await helloResponse.Content.ReadAsStringAsync();
+    Console.WriteLine(helloMessage);
+
+    var data = new UserAccount(){ Name = "Tommaso" };
+    var registerUserResponse = await httpClient.PostAsJsonAsync($"{baseUrl}/invoke/{appId}/method/registerUser", data);
+    registerUserResponse.EnsureSuccessStatusCode();
+    var userAccount = await registerUserResponse.Content.ReadFromJsonAsync<UserAccount>();
+    if(userAccount != null)
+    {
+        Console.WriteLine(userAccount.ToString());
+    }
+}
 
 async Task ReadUsingHttpDaprClientAsync()
 {
-    var httpDaprClient = DaprClient.CreateInvokeHttpClient(appId: "server");
+    using var httpDaprClient = DaprClient.CreateInvokeHttpClient(appId: "server");
 
     var helloResponse = new HttpResponseMessage();
+    var helloMessage = "";
+
     helloResponse = await httpDaprClient.GetAsync("");
-    if(helloResponse.IsSuccessStatusCode)
-    {
-        var helloMessage = await helloResponse.Content.ReadAsStringAsync();
-        Console.WriteLine(helloMessage);
-    }
+    helloResponse.EnsureSuccessStatusCode();
+    helloMessage = await helloResponse.Content.ReadAsStringAsync();
+    Console.WriteLine(helloMessage);
 
     var name = "Tommaso";
     helloResponse = await httpDaprClient.GetAsync($"hello/{name}");
-    if(helloResponse.IsSuccessStatusCode)
-    {
-        var helloMessage = await helloResponse.Content.ReadAsStringAsync();
-        Console.WriteLine(helloMessage);
-    }
+    helloResponse.EnsureSuccessStatusCode();
+    helloMessage = await helloResponse.Content.ReadAsStringAsync();
+    Console.WriteLine(helloMessage);
 
     var data = new UserAccount(){ Name = "Tommaso" };
     var registerUserResponse = await httpDaprClient.PostAsJsonAsync("registerUser", data);
-    if(registerUserResponse.IsSuccessStatusCode)
+    registerUserResponse.EnsureSuccessStatusCode();
+    var userAccount = await registerUserResponse.Content.ReadFromJsonAsync<UserAccount>();
+    if(userAccount != null)
     {
-        var userAccount = await registerUserResponse.Content.ReadFromJsonAsync<UserAccount>();
-        if(userAccount != null)
-        {
-            Console.WriteLine(userAccount.ToString());
-        }
+        Console.WriteLine(userAccount.ToString());
     }
 }
 
