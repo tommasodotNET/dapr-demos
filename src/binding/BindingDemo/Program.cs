1dbaa491ae97c8b6ab834f364a345421c6ecda63
Tommaso Stocchi <tommasostocchi@outlook.com>
2022-11-16 15:32:35 +0000

    using localstorage for binding demo

--- src/binding/BindingDemo/Program.cs
+++ src/binding/BindingDemo/Program.cs
@@ -5,27 +5,20 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using Dapr.Client;
 
-var sqlBindingName = "sqldb";
+var fileBindingName = "files";
 
 using var daprClient = new DaprClientBuilder().Build();
 
-Console.WriteLine("Processing batch..");
+await Task.Delay(10000);
 
-string jsonFile = File.ReadAllText("../orders.json");
-var ordersArray = JsonSerializer.Deserialize<Orders>(jsonFile);
+Console.WriteLine("Creating file...");
 
-foreach(Order ord in ordersArray?.orders ?? new Order[] {}){
-    var sqlText = $"insert into orders (orderid, customer, price) values ({ord.OrderId}, '{ord.Customer}', {ord.Price});";
-    var command = new Dictionary<string,string>(){
-        {"sql",
-        sqlText}
-    };
+var data = "Hello World!";
+var metadata = new Dictionary<string, string>()
+{
+    { "{{itemName}}", "myTestFile.txt" }
+};
 
-    // Insert order using Dapr output binding via Dapr Client SDK
-    await daprClient.InvokeBindingAsync(bindingName: sqlBindingName, operation: "exec", data: "", metadata: command);
-}
+await daprClient.InvokeBindingAsync(bindingName: fileBindingName, operation: "create", data: data, metadata: metadata );
 
-Console.WriteLine("Finished processing batch");
-
-public record Order([property: JsonPropertyName("orderid")] int OrderId, [property: JsonPropertyName("customer")] string Customer, [property: JsonPropertyName("price")] float Price);
-public record Orders([property: JsonPropertyName("orders")] Order[] orders);
+Console.WriteLine("Finished creating file");
